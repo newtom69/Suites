9aea073a34c6be5597addbe97d483def38fcdd63
newtom69 <thomas@vuille.fr>
2018-11-05 07:48:26 +0100

    Fibonacci ok

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fibonacci
 {
@@ -10,6 +8,24 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
+            int max = 45;
+            int i = 0;
+            foreach (var nb in Fibonacci().Take(max))
+            {
+                Console.WriteLine($"entier {++i} : {nb}");
+            }
+        }
+
+        public static IEnumerable<double> Fibonacci()
+        {
+            int courant = 1;
+            int suivant = 1;
+            while (true)
+            {
+                suivant = courant + (courant = suivant);
+                yield return courant;
+            }
+           
         }
     }
 }
